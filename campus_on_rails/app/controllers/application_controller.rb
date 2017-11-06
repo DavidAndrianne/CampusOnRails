167ae23d6bfcbe02fdf4e576298031105a20b20c
DavidAndrianne <r0426942@gmail.com>
2017-11-06 14:04:20 +0100

    1.3 Exercise solutions

--- campus_on_rails/app/controllers/application_controller.rb
+++ campus_on_rails/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hola, mundo! <a href=\"/goodbye\"></a>"
+  end
+
+  def goodbye
+    render html: "Hasta la vista, baby!"
+  end
 end
